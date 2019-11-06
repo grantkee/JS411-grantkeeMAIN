692fb894b8d0326e4b9a6973adc8cd3a7bac4787
grantkee <grant.d.kee@gmail.com>
2019-11-06 15:44:54 -0600

    pushing to pull on my other computer

---
+++ news-feed/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+class NewsFeed extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {  }
+  }
+  render() { 
+    return (  );
+  }
+}
+ 
+export default NewsFeed;
