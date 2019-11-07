c9bd03e687b898bc1f6df1e160141b67fc9d0f71
grantkee <grant.d.kee@gmail.com>
2019-11-07 14:50:39 -0600

    starting app, trying to think about creating search functions.

--- news-feed/src/App.js
+++ news-feed/src/App.js
@@ -4,8 +4,26 @@ import './App.css';
 class NewsFeed extends Component {
   constructor(props) {
     super(props);
-    this.state = {  }
+    this.state = { 
+      term: '',
+      author: '',
+      date: '',
+      errorMsg: '',
+      news: []
+     }
   }
+
+  onChange = (event) => {
+    const result = event.target.type === 'checkbox' ? event.target.checked : event.target.value;
+    const name = event.target.name;
+
+    this.setState({
+      [name] : result
+    });
+  }
+
+  
+
   render() { 
     return (  );
   }
